2c4239ccc59e5a425a7333135e486938193626bd
Martin Ramos <marprg@gmail.com>
2017-09-04 20:59:11 -0300

    cambio de carpeta

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -33,7 +33,7 @@ class UserController extends FOSRestController
 	 {
 	   $singleresult = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
 	   if ($singleresult === null) {
-	   return new View("user not found", Response::HTTP_NOT_FOUND);
+	   return new View("user no encontrado", Response::HTTP_NOT_FOUND);
 	   }
 	 return $singleresult;
 	 }
@@ -44,19 +44,19 @@ class UserController extends FOSRestController
 	 public function postAction(Request $request)
 	 {
 		$data = new User;
-		
+
 		$name = $request->get('name');
 		$role = $request->get('role');
 		if(empty($name) || empty($role))
 		{
-	   		return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE); 
-	 	} 
+	   		return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+	 	}
 		$data->setName($name);
 		$data->setRole($role);
 		$em = $this->getDoctrine()->getManager();
 		$em->persist($data);
 		$em->flush();
-		
+
 		return new View("User Added Successfully", Response::HTTP_OK);
 	 }
 
@@ -64,7 +64,7 @@ class UserController extends FOSRestController
 	 * @Rest\Put("/user/{id}")
 	 */
 	 public function updateAction($id,Request $request)
-	 { 
+	 {
 	 $data = new User;
 	 $name = $request->get('name');
 	 $role = $request->get('role');
@@ -72,7 +72,7 @@ class UserController extends FOSRestController
 	 $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
 	if (empty($user)) {
 	   return new View("user not found", Response::HTTP_NOT_FOUND);
-	 } 
+	 }
 	elseif(!empty($name) && !empty($role)){
 	   $user->setName($name);
 	   $user->setRole($role);
@@ -87,8 +87,8 @@ class UserController extends FOSRestController
 	elseif(!empty($name) && empty($role)){
 	 $user->setName($name);
 	 $sn->flush();
-	 return new View("User Name Updated Successfully", Response::HTTP_OK); 
+	 return new View("User Name Updated Successfully", Response::HTTP_OK);
 	}
-	else return new View("User name or role cannot be empty", Response::HTTP_NOT_ACCEPTABLE); 
+	else return new View("User name or role cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
 	 }
 }
